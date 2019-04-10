5e39aed7e5935678f071f376157ecbcaee219b04
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-10 15:39:52 -0500

    Added onclick handler for expanding contact info

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends React.Component {
+    state = {};
+
+    onExpand = (e) => {
+        console.log(e.target);
+    }
+
     render() {
         const { name, email, phone } = this.props.contact;
         return (
             <div className='card card-body mb-3'>
-                <h4>{name}</h4>
+                <h4>
+                    {name} <i onClick={this.onExpand} className='fas fa-sort-down' />
+                </h4>
                 <ul className='list-group'>
                     <li className='list-group-item'>Email: {email}</li>
                     <li className='list-group-item'>Phone: {phone}</li>
