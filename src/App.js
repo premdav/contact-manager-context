7bc2d93e8c4e08fa3f0d508b07a9ec9b47007153
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-10 10:17:14 -0500

    Basic Header component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Contact from './Components/Contact';
+import Header from './Components/Header';
 
 import './App.css';
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The app component</h1>
+        <Header />
         <Contact />
       </div>
     );
