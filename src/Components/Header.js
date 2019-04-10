4d8dff810fb08636b421eda3b1ac2424e03dfaaa
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-10 10:56:42 -0500

    Added default prop and proptype to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = (props) => {
     return (
@@ -6,6 +7,14 @@ const Header = (props) => {
             <h1>{ props.branding }</h1>
         </div>
     );
+};
+
+Header.defaultProps = {
+    branding: 'Contact Manager'
 }
+
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+};
 
 export default Header;
