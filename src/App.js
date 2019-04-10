dd2874cf62fcd54dc0dea35cd6df15aac6dfc9d4
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-10 10:52:26 -0500

    Passing props to both header and contact components

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding='Contact Manager' />
+        <Contact name='John Doe' email='jdoe@gmail.com' phone='555-555-5555' />
+        <Contact name='Karen Smith' email='ksmith@gmail.com' phone='444-555-5555' />
       </div>
     );
   }
