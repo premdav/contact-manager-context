fe698dec24b54a3d97398e177e71745d20f59044
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-10 11:58:12 -0500

    Simple bootstrap styling added to components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './Components/Contact';
 import Header from './Components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding='Contact Manager' />
-        <Contact name='John Doe' email='jdoe@gmail.com' phone='555-555-5555' />
-        <Contact name='Karen Smith' email='ksmith@gmail.com' phone='444-555-5555' />
+        <div className='container'>
+          <Contact name='John Doe' email='jdoe@gmail.com' phone='555-555-5555' />
+          <Contact name='Karen Smith' email='ksmith@gmail.com' phone='444-555-5555' />
+        </div>
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types';
 
 const Header = (props) => {
     return (
-        <div>
-            <h1>{ props.branding }</h1>
-        </div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+            <div className='container'>
+                <a href='/' className='navbar-brand'>{props.branding}</a>
+                <div>
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='nav-item'>
+                            <a href='/' className='nav-link'>Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
