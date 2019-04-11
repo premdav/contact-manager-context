c938e59203e187d9b7cbef7ad8a7bb8fc1d471ed
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-11 11:59:49 -0500

    Added toggle feature to contact components

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,23 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends React.Component {
-    state = {};
+    state = {
+        showContactInfo: false
+    };
 
     onExpand = (e) => {
-        console.log(e.target);
+        this.setState({ showContactInfo: !this.state.showContactInfo });
     }
 
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
+
         return (
             <div className='card card-body mb-3'>
                 <h4>
                     {name} <i onClick={this.onExpand} className='fas fa-sort-down' />
                 </h4>
-                <ul className='list-group'>
+                {showContactInfo ? (
+                    <ul className='list-group'>
                     <li className='list-group-item'>Email: {email}</li>
                     <li className='list-group-item'>Phone: {phone}</li>
                 </ul>
+                ) : null }
             </div>
         )
     }
