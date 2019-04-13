98bf26af134be733dd4a2dede8812ecf36a446ac
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-04-13 13:26:12 -0500

    Added about page, updated heading on Contact page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './Components/contacts/Contacts';
 import Header from './Components/layout/Header';
 import AddContact from './Components/contacts/AddContact';
+import About from './Components/pages/About';
 
 import { Provider } from './Context';
 
@@ -12,15 +14,19 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding='Contact Manager' />
-          <div className='container'>
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding='Contact Manager' />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Contacts} />
+                <Route exact path='/contact/add' component={AddContact} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
-      
     );
   }
 }
--- src/Components/contacts/Contacts.js
+++ src/Components/contacts/Contacts.js
@@ -11,6 +11,9 @@ class Contacts extends React.Component {
                     const { contacts } = value;
                     return (
                         <React.Fragment>
+                            <h1 className='display-4 mb-2'>
+                                <span className='text-danger'>Contact</span> List
+                            </h1>
                             { contacts.map((contact) => {
                             return (
                                 <Contact 
